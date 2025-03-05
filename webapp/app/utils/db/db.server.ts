1673e9b6bdac0189c0bce7d4760b552b253ed6db
Dan Orzechowski <orzechod@gmail.com>
2025-03-04 20:56:57 -0500

    swap out postgres, swap in pg

--- webapp/app/utils/db/db.server.ts
+++ webapp/app/utils/db/db.server.ts
@@ -1,8 +1,57 @@
-import postgres from "postgres"
+import pg, { type QueryResultRow } from "pg"
 
-const sql = postgres(String(process.env.POSTGRES_CONNECTION_STRING), {
-	password: process.env.POSTGRES_PASSWORD,
-	username: process.env.POSTGRES_USERNAME
+const {
+	Pool
+} = pg
+
+const pool = new Pool({
+	connectionString: process.env.POSTGRES_CONNECTION_STRING
 })
 
-export default sql
+function expandSelectQuery(obj: Record<string, unknown>): [string, string[]] {
+	const x = Object.entries(obj).reduce<[string[], string[]]>(
+		(acc, [k,v], idx) => {
+			acc[0].push(`${k} = $${idx + 1}`)
+			acc[1].push(String(v))
+
+			return acc
+		},
+		[[], []]
+	)
+
+	return [
+		x[0].join(" AND "),
+		x[1]
+	]
+}
+
+function expandInsertQuery(obj: Record<string, unknown>): [string, string, string[]] {
+	const x = Object.entries(obj).reduce<[string[], string[], string[]]>(
+		(acc, [k, v], idx) => {
+			acc[0].push(k)
+			acc[1].push(`${idx}`)
+			acc[2].push(String(v))
+
+			return acc
+		},
+		[[], [], []]
+	)
+
+	return [
+		x[0].join(", "),
+		x[1].join(", "),
+		x[2]
+	]
+}
+
+async function runQuery<T extends QueryResultRow>(queryString: string, values?: string[]) {
+	const result = await pool.query<T>(queryString, values)
+
+	return result.rows
+}
+
+export {
+	expandInsertQuery,
+	expandSelectQuery,
+	runQuery
+}
