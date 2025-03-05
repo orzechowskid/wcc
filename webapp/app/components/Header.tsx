d5166293263f22d5f95bc61c9c074c28dcb09ace
Dan Orzechowski <orzechod@gmail.com>
2025-03-04 20:58:19 -0500

    show current user, or login link, in header

--- webapp/app/components/Header.tsx
+++ webapp/app/components/Header.tsx
@@ -5,12 +5,26 @@ import {
 	styled
 } from "styled-components"
 
+import type {
+	UserRecord
+} from "~/types.ts"
+
+type HeaderProps = {
+	currentUser?: UserRecord
+}
+
 const StyledHeader = styled.header`
+		display: flex;
+		justify-content: space-between;
 		color: var(--color-header-text);
 		background-color: var(--background-color-header);
 `
 
-export default function Header() {
+export default function Header(props: HeaderProps) {
+	const {
+		currentUser
+	} = props
+
 	return (
 		<StyledHeader>
 			<nav>
@@ -20,6 +34,11 @@ export default function Header() {
 					</li>
 				</ul>
 			</nav>
+			{currentUser ? (
+				<Link to="/logout">log out</Link>
+			) : (
+				<Link to="/login">log in</Link>
+			)}
 		</StyledHeader>
 	)
 }
